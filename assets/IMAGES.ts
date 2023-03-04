a395ab20c4b6b10fcddaef2e0c25f92a5980d453
dylankhiew <dylankhiew@gmail.com>
2023-03-04 19:55:44 +0800

    chore: add assets for icons

--- assets/IMAGES.ts
+++ assets/IMAGES.ts
@@ -1,7 +1,17 @@
+import FORK_ICON from "./images/fork_icon.png";
+import MORE_OPTIONS_ICON from "./images/more_options_icon.png";
 import NO_INTERNET_CONNECTION from "./images/no_internet_connection.png";
+import REFRESH_ICON from "./images/refresh_icon.png";
+import STAR_ICON from "./images/star_icon.png";
 
 export const IMAGES = {
   BACKGROUND: {
     NO_INTERNET_CONNECTION,
+  },
+  ICON: {
+    FORK: FORK_ICON,
+    MORE_OPTIONS: MORE_OPTIONS_ICON,
+    REFRESH: REFRESH_ICON,
+    STAR: STAR_ICON,
   },
 };
