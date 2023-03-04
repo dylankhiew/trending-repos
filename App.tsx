adc7bd0bd6333116b2c3cf64cd8f7c5d92649977
dylankhiew <dylankhiew@gmail.com>
2023-03-04 23:38:28 +0800

    feat: add and initialise fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
 
+import { FONTS } from "./assets/FONTS";
 import TrendingScreen from "./src/screens/TrendingScreen/TrendingScreen";
 
 export type NavigationStackParamList = {
@@ -10,6 +12,16 @@ export type NavigationStackParamList = {
 const Stack = createNativeStackNavigator<NavigationStackParamList>();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": FONTS.ROBOTO.BOLD,
+    "Roboto-Medium": FONTS.ROBOTO.MEDIUM,
+    "Roboto-Regular": FONTS.ROBOTO.REGULAR,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
---
+++ assets/FONTS.ts
@@ -0,0 +1,11 @@
+import ROBOTO_BOLD from "./fonts/Roboto-Bold.ttf";
+import ROBOTO_MEDIUM from "./fonts/Roboto-Medium.ttf";
+import ROBOTO_REGULAR from "./fonts/Roboto-Regular.ttf";
+
+export const FONTS = {
+  ROBOTO: {
+    BOLD: ROBOTO_BOLD,
+    MEDIUM: ROBOTO_MEDIUM,
+    REGULAR: ROBOTO_REGULAR,
+  },
+};
