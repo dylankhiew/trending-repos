fa8b161d1612f9bd78c8a2d963b6c9a30104abf7
dylankhiew <dylankhiew@gmail.com>
2023-03-05 19:01:34 +0800

    feat: update saga to include loading and error update

--- src/sagas/reposSagas.ts
+++ src/sagas/reposSagas.ts
@@ -1,21 +1,27 @@
-import { call, put, takeEvery } from "@redux-saga/core/effects";
+import { call, put, select, takeEvery } from "@redux-saga/core/effects";
 
 import { getApi } from "./apiSagas";
-import { requestRepoUpdate, updateRepos } from "../actions/reposActions";
+import {
+  requestRepoUpdate,
+  setIsError,
+  setIsLoading,
+  updateRepos,
+} from "../actions/reposActions";
 import { API_ENDPOINTS } from "../constants/apiConstants";
 
 function* updateReposSaga() {
+  // TO-DO: Do Should update check here
+  yield put(setIsLoading(true));
   try {
     const repos: app.RepositoryItem[] = yield call(
       getApi,
       API_ENDPOINTS.TRENDING_URL
     );
-
-    console.log("success");
     yield put(updateRepos(repos));
+    yield put(setIsLoading(false));
   } catch {
-    // Do nothing for now
-    console.log("failed");
+    yield put(setIsLoading(false));
+    yield put(setIsError(true));
   }
 }
 
