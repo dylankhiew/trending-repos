1d1785f6572c39f1dbbd9ad9ec36592241615fc3
dylankhiew <dylankhiew@gmail.com>
2023-03-05 18:13:12 +0800

    feat: add actions for saga consumption

--- src/actions/reposActions.ts
+++ src/actions/reposActions.ts
@@ -1,9 +1,11 @@
 import { createAction } from "@reduxjs/toolkit";
 
+export const REQUEST_REPO_UPDATE = "REQUEST_REPO_UPDATE";
 export const UPDATE_REPOS = "UPDATE_REPOS";
 export const SET_LAST_UPDATED = "SET_LAST_UPDATED";
 
+const requestRepoUpdate = createAction(REQUEST_REPO_UPDATE);
 const updateRepos = createAction<app.RepositoryItem[]>(UPDATE_REPOS);
 const setLastUpdated = createAction<string>(SET_LAST_UPDATED);
 
-export { updateRepos, setLastUpdated };
+export { requestRepoUpdate, updateRepos, setLastUpdated };
