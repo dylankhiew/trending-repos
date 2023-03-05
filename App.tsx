2ebda7f37fd55e4c74007127abde02723a4b6e44
dylankhiew <dylankhiew@gmail.com>
2023-03-05 17:31:57 +0800

    chore: configure rootReducer and wrap store in <App />

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { FONTS } from "./assets/FONTS";
 import TrendingScreen from "./src/screens/TrendingScreen/TrendingScreen";
+import { persistor, store } from "./src/state/store";
 
 export type NavigationStackParamList = {
   Trending: undefined;
@@ -24,10 +27,14 @@ export default function App() {
   }
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Trending" component={TrendingScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Trending" component={TrendingScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
---
+++ src/state/rootReducer.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import { reposReducers } from "./reposReducers";
+
+export const rootReducer = combineReducers({
+  repos: reposReducers,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
