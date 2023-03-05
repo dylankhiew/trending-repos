187632d0a0f86fc0bb717b9b36c5f993d1209c7c
dylankhiew <dylankhiew@gmail.com>
2023-03-05 14:12:26 +0800

    refactor: update COLOR constant

--- src/constants/colorConstants.ts
+++ src/constants/colorConstants.ts
@@ -1,4 +1,7 @@
 export const COLOR = {
-  GRAY: "#E8E8E8",
-  DARK_GRAY: "#52575C",
+  GRAY100: "#E8E8E8",
+  GRAY200: "#929292",
+  GRAY300: "#52575C",
+  WHITE: "#FFFFFF",
+  GREEN: "#31B057",
 };
