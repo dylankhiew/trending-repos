1b91a63fd4904679d584b1ea9ffaeca7f7b6aaf9
dylankhiew <dylankhiew@gmail.com>
2023-03-05 17:30:39 +0800

    feat: configure Redux store

---
+++ src/state/store.ts
@@ -0,0 +1,26 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { persistReducer, persistStore } from "redux-persist";
+import createSagaMiddleware from "redux-saga";
+
+import { rootReducer } from "./rootReducer";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [sagaMiddleware],
+});
+
+export const persistor = persistStore(store);
+
+export type AppDispatch = typeof store.dispatch;
+export const useReduxDispatch = (): AppDispatch => useDispatch<AppDispatch>();
