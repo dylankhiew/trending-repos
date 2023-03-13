857ada970a3ad65a8fe1dc8ff1ef8561a30b93b8
dylankhiew <dylankhiew@gmail.com>
2023-03-13 12:07:42 +0800

    feat: add actions to toggle and sort repo

--- src/actions/reposActions.ts
+++ src/actions/reposActions.ts
@@ -8,6 +8,10 @@ export const SET_IS_ERROR = 'SET_IS_ERROR';
 
 export const SET_LAST_UPDATED = 'SET_LAST_UPDATED';
 
+export const TOGGLE_DISPLAY_SORT_MENU = 'TOGGLE_DISPLAY_SORT_MENU';
+
+export const SET_REPO_SORTING_METHOD = 'SET_REPO_SORTING_METHOD';
+
 const requestRepoUpdate =
   createAction<app.RequestRepoUpdatePayload>(REQUEST_REPO_UPDATE);
 const updateRepos = createAction<app.RepositoryItem[]>(UPDATE_REPOS);
@@ -17,10 +21,18 @@ const setIsError = createAction<boolean>(SET_IS_ERROR);
 
 const setLastUpdated = createAction<string>(SET_LAST_UPDATED);
 
+const toggleDisplaySortMenu = createAction(TOGGLE_DISPLAY_SORT_MENU);
+
+const setRepoSortingMethod = createAction<app.RepoSortingMethod>(
+  SET_REPO_SORTING_METHOD,
+);
+
 export {
   requestRepoUpdate,
   updateRepos,
   setIsLoading,
   setIsError,
   setLastUpdated,
+  toggleDisplaySortMenu,
+  setRepoSortingMethod,
 };
